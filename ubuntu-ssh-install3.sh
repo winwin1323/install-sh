57d4a561c337775c92bb521715825957bddf5c9f
winwin1323 <95872326+winwin1323@users.noreply.github.com>
2023-03-24 16:46:46 +0900

    Create ubuntu-ssh-install3.sh

---
+++ ubuntu-ssh-install3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# ufw 방화벽 활성화
+sudo apt update
+sudo apt-get install -y ufw
+sudo ufw allow 19001/tcp
+sudo ufw --force enable
+# SSH 설치
+sudo apt-get install -y openssh-server
+# SSH 포트 변경
+sudo sed -i 's/#Port 22/Port 19001/g' /etc/ssh/sshd_config
+# SSH 서비스 재시작
+sudo systemctl restart sshd.service

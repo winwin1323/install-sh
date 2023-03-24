123171e1a17ba4cbbffce090a056841529e4f446
winwin1323 <95872326+winwin1323@users.noreply.github.com>
2023-03-24 15:31:56 +0900

    Update ubuntu-ssh-install.sh

--- ubuntu-ssh-install.sh
+++ ubuntu-ssh-install.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 # ufw 방화벽 활성화
-sudo systemctl enable ufw
-sudo systemctl start ufw
-
-# ufw 방화벽에 19001 포트 추가
+sudo apt-get update
+sudo apt-get install -y ufw
 sudo ufw allow 19001/tcp
+sudo ufw --force enable
 
 # SSH 설치
-sudo apt-get install openssh-server
+sudo apt-get install -y openssh-server
 
 # SSH 포트 변경
 sudo sed -i 's/#Port 22/Port 19001/g' /etc/ssh/sshd_config
 
 # SSH 서비스 재시작
-sudo service ssh restart
-
-
+sudo systemctl restart sshd.service

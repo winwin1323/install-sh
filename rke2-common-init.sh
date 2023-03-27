84a4e07cecd5a3c6e16ccc881691bf1084e1b6c2
winwin1323 <95872326+winwin1323@users.noreply.github.com>
2023-03-27 16:37:26 +0900

    Add files via upload

--- rke2-common-init.sh
+++ rke2-common-init.sh
@@ -5,3 +5,18 @@
 sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-add-repository "deb https://apt.kubernetes.io/ kubernetes-xenial main"
+
+sudo apt-get update && sudo apt-get install -y kubectl
+
+
+# Check if directory exists
+if [ ! -d /$HOME/.kube ]; then
+  sudo mkdir -p /$HOME/.kube
+fi
+
+# Copy kubectl config to .kube directory
+sudo cp /etc/rancher/rke2/rke2.yaml /$HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config

3966a4bf1214c61c2170a87c0ee8374550463242
winwin1323 <95872326+winwin1323@users.noreply.github.com>
2023-03-28 16:18:32 +0900

    Add files via upload

---
+++ rke2-main-master-install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#RKE2 https://github.com/rancher/rke2/releases version 확인하는곳
+
+# RKE2 Config
+if [ ! -f /etc/rancher/rke2/config.yaml ]; then
+    mkdir -p /etc/rancher/rke2/
+fi
+	
+cat << EOF > /etc/rancher/rke2/config.yaml
+write-kubeconfig-mode: "0644"
+tls-san:
+  - "10.253.8.82"
+  - "10.253.8.44"
+  - "10.253.8.46"
+server: https://10.253.8.82:9345
+EOF
+
+
+# Install RKE2
+curl -sfL https://get.rke2.io | INSTALL_RKE2_VERSION="v1.26.2+rke2r1" sh -
+
+sleep 3
+
+sudo systemctl enable rke2-server.service
+sudo systemctl start rke2-server.service

b4326f269ab2336a48dba74441386de0f8442291
winwin1323 <95872326+winwin1323@users.noreply.github.com>
2023-03-28 10:11:04 +0900

    Add files via upload

---
+++ rke2-master-ha-install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Get node token
+
+sudo systemctl stop rke2-server.service
+
+#TOKEN - sudo cat /var/lib/rancher/rke2/server/node-token
+
+TOKEN=$1
+
+# Create YAML file for cluster configuration if it does not exist
+if [ ! -f /etc/rancher/rke2/config.yaml ]; then
+    mkdir -p /etc/rancher/rke2/
+fi
+	
+cat << EOF > /etc/rancher/rke2/config.yaml
+write-kubeconfig-mode: "0644"
+tls-san:
+  - "10.253.8.82"
+  - "10.253.8.44"
+server: https://10.253.8.82:9345
+token: "$TOKEN"
+EOF
+fi
+
+sudo systemctl start rke2-server.service
+

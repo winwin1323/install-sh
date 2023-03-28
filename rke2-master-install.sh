e07cd50705443fcef6028aaf82ba7b228dacd4a4
winwin1323 <95872326+winwin1323@users.noreply.github.com>
2023-03-28 14:48:58 +0900

    Update rke2-master-install.sh

--- rke2-master-install.sh
+++ rke2-master-install.sh
@@ -6,5 +6,7 @@
 # Install RKE2
 curl -sfL https://get.rke2.io | INSTALL_RKE2_VERSION="v1.26.2+rke2r1" sh -
 
+sleep 3
+
 sudo systemctl enable rke2-server.service
 sudo systemctl start rke2-server.service

1fd08f88527f1190a4b18db8d3164712f2f05ee5
winwin1323 <95872326+winwin1323@users.noreply.github.com>
2023-03-29 11:06:26 +0900

    Add files via upload

--- rke2-worker-install.sh
+++ rke2-worker-install.sh
@@ -19,5 +19,5 @@ curl -sfL https://get.rke2.io | INSTALL_RKE2_VERSION="v1.26.2+rke2r1" INSTALL_RK
 
 sleep 3
 
-sudo systemctl enable rke2-server.service
-sudo systemctl start rke2-server.service
+sudo systemctl enable rke2-agent
+sudo systemctl start rke2-agent

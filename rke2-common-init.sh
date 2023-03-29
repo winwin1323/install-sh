698acdfadc461b778ce019efff2a146651b2b9cf
winwin1323 <95872326+winwin1323@users.noreply.github.com>
2023-03-29 12:35:48 +0900

    Add files via upload

--- rke2-common-init.sh
+++ rke2-common-init.sh
@@ -7,3 +7,4 @@ sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
 # ubutu 20 자동업데이트 끄기
 sudo sed -i 's/APT::Periodic::Update-Package-Lists "1";/APT::Periodic::Update-Package-Lists "0";/g' /etc/apt/apt.conf.d/20auto-upgrades
+sudo sed -i 's/APT::Periodic::Unattended-Upgrade "1";/APT::Periodic::Unattended-Upgrade "0";/g' /etc/apt/apt.conf.d/20auto-upgrades

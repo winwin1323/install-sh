25a6ba4380f9b57af01561ebc73d8461b63cc9b7
winwin1323 <95872326+winwin1323@users.noreply.github.com>
2023-04-03 09:55:32 +0900

    Update rke2-harbor-install.sh

--- rke2-harbor-install.sh
+++ rke2-harbor-install.sh
@@ -7,7 +7,7 @@ kubectl get namespace harbor-system &> /dev/null || kubectl create namespace har
 helm repo add harbor https://helm.goharbor.io
 helm repo update
 
-helm install --wait --name harbor --namespace harbor-system harbor/harbor \
+helm install harbor --namespace harbor-system harbor/harbor \
   --set expose.type=nodePort \
   --set expose.tls.auto.commonName=$HARBOR_HOSTNAME \
   --set externalURL=https://$HARBOR_HOSTNAME:30003 \
